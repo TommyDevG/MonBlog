7a05a8671b2be5828fc73ce4c6a93b6cefc46e67
thomas guillier <83942814+TommyDevG@users.noreply.github.com>
2022-06-16 15:26:42 +0200

    Delete Mon Blog directory

--- Mon Blog/pages/home.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($db->query("SELECT * FROM articles", 'App\Table\Article') as $post): ?>
-     
-    <h2><a href="<?= $post->getURL(); ?>"><?= $post->titre; ?></a></h2>
-    <p>Publié il y a <?= $post->getTimeSincePost();?></p>
-    <p><?= $post->getExtrait(); ?></p>
-
-<?php endforeach; ?>
